451d3e8cae44146a0584dd465d6b27eb9906593e
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-07-18 22:57:19 +0300

    Подключает и настраивает Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 export function App() {
     return (
         <div>
-            
+            <h1 className="font-bold">Hello, React!</h1>
+            <h1>Hello React!</h1>
         </div>
     );
 }
