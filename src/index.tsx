25e98af3482ca73bf76d82dd50b5d4371a9b57c1
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-07-18 23:55:30 +0300

    Добавляет store

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import { App } from './App';
+import { App } from "./App";
+import {store} from './store';
 
-import './index.css';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
 
 root.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
+    </Provider>
 );
